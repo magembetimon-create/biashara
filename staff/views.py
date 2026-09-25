7b09077df740d32387a0aadf2f3c72120d038564
Nkuba <magembetimon@gmail.com>
2026-09-25 09:22:16 +0300

    changes

--- staff/views.py
+++ staff/views.py
@@ -1122,10 +1122,10 @@ def shift_actor_sales(request):
         if all_staff:
             actor_name = 'Wafanyakazi wote' if todo.get('useri') and getattr(todo.get('useri'), 'langSet', 1) == 0 else 'All staff'
         else:
-            actor = InterprisePermissions.objects.filter(pk=actor_id, Interprise=duka.id).select_related(
-                'user__user', 'fanyakazi', 'user_entp__Interprise'
-            ).first()
-            if not actor:
+        actor = InterprisePermissions.objects.filter(pk=actor_id, Interprise=duka.id).select_related(
+            'user__user', 'fanyakazi', 'user_entp__Interprise'
+        ).first()
+        if not actor:
                 return render(request, 'errorpage.html', todo)
             actor_name = staff_display_name(actor)
 
